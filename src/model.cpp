b586cd763f51e82fce130f74c01e67a0ac457654
Lorenzo Contento <lcontento@users.noreply.github.com>
2020-11-15 00:23:13 +0100

    debug

--- src/model.cpp
+++ src/model.cpp
@@ -11,7 +11,7 @@
 #include <regex>
 #include <typeinfo>
 #include <utility>
-#include <cstdio>
+#include <iostream>
 
 namespace amici {
 
@@ -828,7 +828,7 @@ void Model::addObservableObjective(realtype &Jy, const int it,
                 state_.fixedParameters.data(), y_.data(), sigmay_.data(),
                 edata.getObservedDataPtr(it));
             printf("%f %d %d %f %f\n", nllh.at(0), it, iyt, edata.getTimepoint(it), getTimepoint(it));
-            flush(std::cout);
+            std::flush(std::cout);
             Jy -= nllh.at(0);
         }
     }
