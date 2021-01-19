0bba7f5984aa467ea7b453a79c1e50db4fc500ec
Lorenzo Contento <lcontento@users.noreply.github.com>
2021-01-19 17:23:45 +0100

    sympify before fp-optimization

--- python/amici/ode_export.py
+++ python/amici/ode_export.py
@@ -2970,7 +2970,7 @@ class ODEExporter:
         ))
         try:
             ret = cxxcode(
-                optimize(math, optims_c99), # e.g., log(1 + x) --> logp1(x)
+                optimize(sp.sympify(math), optims_c99), # e.g., log(1 + x) --> logp1(x)
                 standard='c++11',
                 user_functions=user_functions
             )
